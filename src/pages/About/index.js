af74fa5b43ac2d57b7283f27d2df0c47e0afc2c7
Mateus <mateus@Mateuss-MacBook-Air.local>
2020-03-29 02:44:58 -0300

    change icon.fav :rocket:

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import statements from '../../utils/statements';
 
@@ -7,6 +7,14 @@ import logo from '../../assets/logo.svg';
 import girl from '../../assets/girl.png';
 
 export default function About() {
+  const [showWorks, setShowWorks] = useState(false);
+  const [useApp, setUseApp] = useState(false);
+  const [how, setHow] = useState(false);
+  const [mobile, setMobile] = useState(false);
+  const [doubt, setDoubt] = useState(false);
+  const [privacy, setPrivacy] = useState(false);
+  const [stataments, setStatements] = useState(false);
+  
   return (
     <div className="full-content">
       <header>
@@ -17,36 +25,88 @@ export default function About() {
         <span>Leia atentamente sobre essa aplicação.</span>
       </header>
 
-      <h1>Como funciona?</h1>
-      <p>{statements.about}</p>
+      <section className="section-all" 
+        style={{width: "100%", 
+        margin: "200px auto 0px", 
+        display: "block"
+      }}>
+      <div className="toggle-content">
+        <button onClick={() => setShowWorks(!showWorks)} type="button">
+        <span>I.</span>
+        <strong>Como funciona?</strong>
+        </button>
+        {showWorks && <p>{statements.about}</p>}
+      </div>
 
-      <h1>Como usar a aplicação?</h1>
-      <p>{statements.instructionForUse}</p>
+      <div className="toggle-content">
+        <button onClick={() => setUseApp(!useApp)} type="button">
+        <span>II.</span>
+        <strong>Como usar a aplicação?</strong>
+        </button>
+        {useApp && <p>{statements.instructionForUse}</p>}
+      </div>
 
-      <h1>Ficou com dúvida de como utilizar a plataforma web? Assista o vídeo.</h1>
-      <a style={{color: "#fff"}} href="https://youtu.be/yWO2egpa3Gg" target="_blank" rel="noopener">Clique aqui</a>
+      <div className="toggle-content">
+        <button onClick={() => setHow(!how)} type="button">
+        <span>III.</span>
+        <strong>Dúvidas de uso na Web? Assista</strong>
+        </button>
+        {how && 
+        <p>
+          {statements.doubtAboutUse}
+          <a target="_blank" rel="noopener" href="https://www.youtube.com/watch?v=yWO2egpa3Gg&feature=youtu.be">Clique</a>
+        </p>}
+      </div>
 
-      <h1>Instalação da aplicação mobile</h1>
-      <p>{statements.instalationGuides}</p>
+      <div className="toggle-content">
+        <button onClick={() => setMobile(!mobile)} type="button">
+        <span>IV.</span>
+        <strong>Instalação da aplicação mobile</strong>
+        </button>
+        {mobile && 
+        <p>
+          {statements.instalationGuides}
+          <a target="_blank" rel="noopener" href="https://drive.google.com/file/d/12VoNY09jhmiFSKzY3xfiY8U_x6j17kD6/view">Clique aqui para baixar</a>
+        </p>}
+      </div>
 
-      <a style={{color: "#fff"}} href="https://drive.google.com/open?id=12VoNY09jhmiFSKzY3xfiY8U_x6j17kD6" target="_blank" rel="noopener">Clique aqui</a>
 
-      <h1>Ficou com dúvida de como funciona o app mobile? Veja aqui:</h1>
-      <a style={{color: "#fff"}} href="https://youtu.be/hR353K0JE24" target="_blank" rel="noopener">Clique aqui</a>
+      <div className="toggle-content">
+        <button onClick={() => setDoubt(!doubt)} type="button">
+        <span>V.</span>
+        <strong>Dúvidas sobre o app? Assista</strong>
+        </button>
+        {doubt && <p>
+          {statements.doubtMobileUsage}
+          <a target="_blank" rel="noopener" href="https://www.youtube.com/watch?v=hR353K0JE24&feature=youtu.be">Clique</a>
 
-      <h1>Privacidade do Usuário</h1>
-      <p>{statements.userPrivacy}</p>
+        </p>}
+      </div>
 
-      <h1>Avisos contra tentativas de má indole.</h1>
-      <p>{statements.userStatements}</p>
+      <div className="toggle-content">
+        <button onClick={() => setPrivacy(!privacy)} type="button">
+        <span>VI.</span>
+        <strong>Privacidade do Usuário</strong>
+        </button>
+        {privacy && <p>{statements.userPrivacy}</p>}
+      </div>
 
+      <div className="toggle-content">
+        <button onClick={() => setStatements(!stataments)} type="button">
+        <span>VII.</span>
+        <strong>Avisos contra tentativas de má indole</strong>
+        </button>
+        {stataments && <p>{statements.userStatements}</p>}
+      </div>
+      </section>
+     
       <div className="statements-content">
         <h1>Fique em casa</h1>
         <img src={girl} alt="heroes" />
       </div>
 
       <footer>
-        Desenvolvido por M - © 2020
+        Desenvolvido com ❤️ por M - © 2020
         <Link to='/home'>Ir para página inicial</Link>
       </footer>
     </div>
